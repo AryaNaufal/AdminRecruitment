eb7a1676397a73abbb2fef57de0ef9fe908e1cf5
Nastavnic <79558550+AryaNaufal@users.noreply.github.com>
2023-03-29 01:55:57 +0700

    Update Signup.php

--- function/Signup.php
+++ function/Signup.php
@@ -15,7 +15,7 @@ if (!empty($mail) && !empty($pass)) {
     $sql_role_name  = mysqli_query($conn, "SELECT role_name FROM user WHERE role_name = 'Administrator'");
     $result_row_name = mysqli_fetch_array($sql_role_name);
     if ($result_row_name['role_name'] <= 0) {
-      $role = mysqli_query($conn, "SELECT role_name FROM user where role_");
+      $role = mysqli_query($conn, "SELECT role_name FROM user where role_name");
       mysqli_query($conn, "INSERT INTO user (role_id, role_name, nama, email, password) VALUES ('', '', '$user', '$mail', '$hash')");
       $id = mysqli_insert_id($conn);
       mysqli_query($conn, "UPDATE user SET role_id = 'ADM_00$id', role_name = 'Administrator' WHERE id=last_insert_id();");
